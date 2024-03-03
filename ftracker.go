19f7d1311838c49c22f9c6872d586b9f662a3068
Kuchmistaya Yana <belousovajana1801@gmail.com>
2024-03-03 14:43:41 +0300

    исправление ошибок

--- ftracker.go
+++ ftracker.go
@@ -102,8 +102,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	speedMC := meanSpeed(action, duration) * kmhInMsec
-	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(speedMC, 2)/height/cmInM)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
+	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow((meanSpeed(action, duration)*kmhInMsec), 2)/height/cmInM)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
